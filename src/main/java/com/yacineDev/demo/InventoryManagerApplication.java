4051bedc526974c14a61f5236b912a83fbba18f0
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-12-26 22:38:15 +0100

    next step is to check if the connection is properly done and the insert method

--- src/main/java/com/yacineDev/demo/InventoryManagerApplication.java
+++ src/main/java/com/yacineDev/demo/InventoryManagerApplication.java
@@ -2,12 +2,23 @@ package com.yacineDev.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class InventoryManagerApplication {
+	@Autowired
+	private ProductService productService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagerApplication.class, args);
-	}
+		InventoryManagerApplication app = new InventoryManagerApplication();
+		try {
+			app.productService.insertProduct("Product 3", 29.99);
 
+		} catch (SQLException e) {
+			// Handle the exception
+		}
+	}
 }
